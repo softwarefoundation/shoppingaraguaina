db79270cef8f3e675bcb32e3e63aa87e8fe7cc4c
Dherkyan Ribeiro <dherkyan@gmail.com>
2023-09-10 01:19:12 -0300

    Configure Swagger

---
+++ src/main/java/br/com/shoppingaraguaina/controller/AppInfoController.java
@@ -0,0 +1,16 @@
+package br.com.shoppingaraguaina.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/app-info")
+public class AppInfoController {
+
+    @GetMapping
+    public String getInfo() {
+        return "Shopping Araguaína";
+    }
+
+}
