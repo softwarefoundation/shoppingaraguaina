1c29fbf28e19066d2bf6ba67c13099571da05f66
Dherkyan Ribeiro <dherkyan@gmail.com>
2023-09-10 00:03:21 -0300

    Inicio projeto Shopping Araguaína

---
+++ src/main/java/br/com/shoppingaraguaina/ShoppingaraguainaApplication.java
@@ -0,0 +1,13 @@
+package br.com.shoppingaraguaina;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingaraguainaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoppingaraguainaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/shoppingaraguaina/ShoppingaraguainaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.shoppingaraguaina;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingaraguainaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
